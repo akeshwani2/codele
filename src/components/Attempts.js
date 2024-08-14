08bf0c0d1f9c8b4c68be63e009a7b0ed4c49c8a8
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2024-08-14 02:19:00 -0400

    Implements basic components and starts a text field with attempt counter

---
+++ src/components/Attempts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Attempts({ count }) {
+  return <p>Attempts left: {count}</p>;
+}
+
+export default Attempts;
---
+++ src/components/Feedback.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Feedback({ message }) {
+  return <p>{message}</p>;
+}
+
+export default Feedback;
