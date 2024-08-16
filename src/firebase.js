ac491e13defe229778436c935efaa9e93e19bf51
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2024-08-16 00:45:01 -0400

    Hide public API key

--- src/firebase.js
+++ src/firebase.js
@@ -1,28 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB82p_o2L7pPkMBAg6B1JC3XUyjdVP_Vls",
-  authDomain: "codele-bfb32.firebaseapp.com",
-  projectId: "codele-bfb32",
-  storageBucket: "codele-bfb32.appspot.com",
-  messagingSenderId: "780299994074",
-  appId: "1:780299994074:web:130ae277498eb00699f6e3",
-  measurementId: "G-NV0SHBRQV0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
-export default firebaseConfig;
 
 export { auth };
