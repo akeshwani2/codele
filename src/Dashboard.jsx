624eee8c2bfe2976cd5c516cace7d44f6b85d8ad
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2024-08-17 08:12:21 -0400

    Implement new landing page, fixed routing. PENDING: Style login/signup pages, output checkin

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
 import "./App.css"
+import { Link, useNavigate } from 'react-router-dom'
+import { Button } from "@chakra-ui/react";
+
 
 const Dashboard = ({className}) => {
+    const navigate = useNavigate()
+
+    const handleLoginClick = () => {
+        navigate("/login");
+    };
+
+    const handleSignUpClick = () => {
+        navigate("/signup")
+    }
+
   return (
     <div className={className}>  
         <div>
             <form>
-                <h2>dash</h2>
-                <label htmlFor="email">
-                    Email:
-                    <input type='text' />
-                </label>
-                <label htmlFor='password'>
-                    Password:
-                    <input type='password' />
-                </label>
-                <button>Sign Up</button> <br />
-                <p>Already Registered? <a>Login</a></p>
+                <div className='dash-t'>
+                <h1>Welcome to Codele!</h1>
+                </div>
+                <div className='dash-p'>
+                <h1>Begin your coding journey today!</h1>
+                </div>
+                <div className='dash-log'>
+                <Button colorScheme="red" bg="#FF1744" p={7} fontSize="2xl" mr={4} onClick={handleLoginClick}>Login</Button>
+                <Button colorScheme="green" bg="#00FF66" p={7} fontSize="2xl" onClick={handleSignUpClick}>Sign Up</Button>
+                </div>
+
+                
             </form>
         </div>
     </div>
--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { auth } from './firebase';
 
 const ProtectedRoute = ({ children }) => {
-  const user = auth.currentUser;
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    // Render a loading indicator while waiting for Firebase to confirm the user's authentication status
+    return <div></div>;
+  }
 
   if (!user) {
     // If no user is authenticated, redirect to the login page
-    return <Navigate to="/login" />;
+    return <Navigate to="/" />;
   }
 
   // If the user is authenticated, render the children (i.e., the protected content)
